1b532db539677dbd32ae262930a8ff0bc06690d4
Hoang Dinh <hoang.dv@outlook.com>
2021-12-18 13:36:49 +0900

    Update: Use cryptocompare api

---
+++ README.md
@@ -0,0 +1,21 @@
+# Cryptocurrency Index
+
+## How to start?
+
+- Install dependency
+
+```
+npm ci
+```
+
+- Start dev server
+
+```
+npm start
+```
+
+- Access `http://127.0.0.1:8080` by a browser
+
+## Refer
+
+- Api source: https://cryptocompare.com
--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,7 @@
 // API dành cho việc lấy thông tin của các đồng tiến số
 // (thông tin chúng ta cần là logo images của các loại tiền)
 // Doc: https://www.cryptocompare.com/api/
-let CRYPTOCOMPARE_API_URI = "https://www.cryptocompare.com";
-
-// API dành cho việc lấy các thông tin quan trọng của top 10 đồng tiền số
-// Doc: https://coinmarketcap.com/api/
-let COINMARKETCAP_API_URI = "https://api.coinmarketcap.com";
+let CRYPTOCOMPARE_API_URI = "https://min-api.cryptocompare.com";
 
 // Thời gian sẽ update lại thông tin đang hiển thị
 let UPDATE_INTERVAL = 60 * 1000;
@@ -14,40 +10,20 @@ let app = new Vue({
     el: "#app",
     data: {
         coins: [], // Array chứ thông tin các đồng tiền khác nhau(bitcoin, ethereum...)
-        coinData: {} // Đối tượng chứa kết quả của API CryptoCompare. Chúng ta sẽ dùng nó để lấy ra thông tin logos
     },
     created: function () {
-        this.getCoinData();
+        this.getCoins();
     },
     methods: {
-
-        /**
-         * Lấy thông tin của tất cả các đồng tiền, thông tin được sử dụng để lấy ra
-         * logo tương ứng của từng đồng tiền.
-         */
-        getCoinData: function () {
-            let self = this;
-            axios.get("./static/coins.json")
-                .then((resp) => {
-                    this.coinData = resp.data.Data;
-                    this.getCoins();
-                })
-                .catch((err) => {
-                    this.getCoins();
-                    console.error(err);
-                });
-        },
-
         /**
          * Lấy thông tin top 10 đồng tiền phổ biến
          * Thông tin sẽ được refresh sau UPDATE_INTERVAL / 1000 seconds
          * bằng cách gọi lại API
          */
         getCoins: function () {
-            let self = this;
-            axios.get(COINMARKETCAP_API_URI + "/v1/ticker/?limit=10")
+            axios.get(CRYPTOCOMPARE_API_URI + "/data/top/totalvolfull?limit=10&tsym=USD")
                 .then((resp) => {
-                    this.coins = resp.data;
+                    this.coins = resp.data.Data;
                 })
                 .catch((err) => {
                     console.error(err);
@@ -57,19 +33,9 @@ let app = new Vue({
         /**
          * Truyền vào ký hiệu của đồng tiền, trả ra logo của đồng tiền đó
          */
-        getCoinImage: function (symbol) {
-            symbol = (symbol === "MIOTA" ? "IOT" : symbol);
-            symbol = (symbol === "VERI" ? "VRM" : symbol);
-            symbol = (symbol === "BCC" ? "BCCOIN" : symbol);
-            if (!this.coinData[symbol]) {
-                console.log('Special symbol', symbol);
-                return;
-            }
-            return CRYPTOCOMPARE_API_URI + this.coinData[symbol].ImageUrl;
+        getCoinImage: function (coin) {
+            return "https://cryptocompare.com" + coin.CoinInfo.ImageUrl;
         },
-        getColor: (num) => {
-            return num > 0 ? "color:green;" : "color:red;";
-        }
     }
 });
 
